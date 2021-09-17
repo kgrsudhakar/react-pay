dda1a3f743eb65345f23c7684c88041d0c29d4c8
kgrsudhakar <kgrsudhakar@gmail.com>
2021-09-18 00:09:54 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-// import './style.css';
-import DataTable from './DataTable.js';
+import './style.css';
+import OutlinedCard from './DataTable.js';
 import Button from '@mui/material/Button';
+import { StyledEngineProvider } from '@mui/material/styles';
 
 export default function App() {
   return (
     <>
-      <DataTable />
+      <OutlinedCard />
+
       <Button variant="contained">Hello World</Button>
     </>
   );
