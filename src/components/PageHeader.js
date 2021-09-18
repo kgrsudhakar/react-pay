b93583e0b42ebe788dc096bf4eb8593e4b6e4df0
kgrsudhakar <kgrsudhakar@gmail.com>
2021-09-19 03:07:02 +0530

    1 file modified

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -6,19 +6,20 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: '#fdfdff',
   },
   pageHeader: {
-    padding: theme.spacing(1),
+    padding: theme.spacing(0),
     display: 'flex',
     marginBottom: theme.spacing(0),
   },
   pageIcon: {
     display: 'inline-block',
-    padding: theme.spacing(1),
+    padding: theme.spacing(0),
     color: '#3c44b1',
   },
   pageTitle: {
-    paddingLeft: theme.spacing(0),
+    paddingLeft: 'theme.spacing(0)',
+    marginLeft: '-20px',
     '& .MuiTypography-subtitle2': {
-      opacity: '0.6',
+      opacity: '0.4',
     },
   },
 }));
@@ -28,13 +29,13 @@ export default function PageHeader(props) {
   const { title, subTitle, icon } = props;
   return (
     <Paper elevation={0} className={classes.root}>
-      <div className={classes.pageHeader}>
-        {/* <Card className={classes.pageIcon}>{icon}</Card> */}
-        <div className={classes.pageTitle}>
-          <Typography variant="h6" component="div">
-            {title}
-          </Typography>
-        </div>
+      {/* <div className={classes.pageHeader}> */}
+      {/* <Card className={classes.pageIcon}>{icon}</Card> */}
+      <div className={classes.pageTitle}>
+        <Typography variant="h6" component="div">
+          {title}
+        </Typography>
+        {/* </div> */}
       </div>
     </Paper>
   );
