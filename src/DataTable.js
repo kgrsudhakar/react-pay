ce7a08ae94ae2d01219bb4d3939197260226c95d
kgrsudhakar <kgrsudhakar@gmail.com>
2021-09-18 16:45:40 +0530

    2 files modified

--- src/DataTable.js
+++ src/DataTable.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -11,10 +10,18 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { experimentalStyled as styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 function createData(name, calories) {
   return { name, calories };
 }
+
+const rowName = '';
+const rowCalories = '';
 
 const rows = [
   createData('Frozen yoghurt', 159),
@@ -40,6 +47,12 @@ const bull = (
 
 function handle(row) {
   console.log('test row click', row);
+
+  const data = {};
+  data = row;
+  rowName = data.name;
+  rowCalories = data.calories;
+  console.log(rowName, rowCalories);
 }
 const card = (
   <React.Fragment>
@@ -72,50 +85,29 @@ const card = (
 
 const modal = (
   <React.Fragment>
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell>Calories</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              onClick={() => handle(row)}
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell>{row.calories}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        {rowName}
+      </Typography>
+      <Typography variant="h5" component="div">
+        {rowCalories}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Add</Button>
+    </CardActions>
   </React.Fragment>
 );
 
-export default function OutlinedCard() {
+export default function DataTable() {
   return (
-    // <Box sx={{ minWidth: 275 }}>
-    //   <Card variant="outlined">{card}</Card>
-    //   <Card variant="outlined">{card}</Card>
-    // </Box>
-    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'block' } }}>
+    <Box sx={{ display: { xs: 'block', md: 'block' } }}>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         <Grid item xs={6}>
-          <Item>
-            <Card>{card}</Card>
-          </Item>
+          <Item>{card}</Item>
         </Grid>
         <Grid item md={6}>
-          <Item>
-            <Card>{modal}</Card>
-          </Item>
+          <Item>{modal}</Item>
         </Grid>
       </Grid>
     </Box>
